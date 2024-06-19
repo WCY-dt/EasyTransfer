8b87c853180d7fde21472b0eaa7fd26670ec8ee9
WCY-dt <55525165+WCY-dt@users.noreply.github.com>
2024-06-19 06:30:18 +0000

    Deploying to gh-pages from @ WCY-dt/EasyTransfer@974009c8a7ebfb20f8838ec48f7eafbdcb3633a2 🚀

--- util.js
+++ util.js
@@ -1,34 +1,9 @@
-fileInput.addEventListener('change', function () {
-    if (this.files.length > 0) {
-        fileName.textContent = this.files[0].name;
-        // 改变字体大小
-        fileName.style.fontSize = '20px';
-    } else {
-        fileName.textContent = '拖拽或点击上传文件';
-    }
-});
-
-dropzone.onclick = () => fileInput.click();
-dropzone.ondragover = event => {
-    event.preventDefault();
-    dropzone.style.backgroundColor = 'lightgray';
-};
-dropzone.ondragleave = () => dropzone.style.backgroundColor = 'white';
-dropzone.ondrop = event => {
-    event.preventDefault();
-    fileInput.files = event.dataTransfer.files;
-    dropzone.style.backgroundColor = 'white';
-    var event = new Event('change');
-    fileInput.dispatchEvent(event);
-};
-
 if (targetIdInput.value.length !== 4) {
     connectButton.style.backgroundColor = 'gray';
     connectButton.disabled = true;
     connectButton.style.cursor = 'not-allowed';
 }
 
-// 当targetIdInput的值改变时，去除connectButton的行内样式，并去除disabled属性
 targetIdInput.addEventListener('input', () => {
     connectButton.removeAttribute('style');
     connectButton.removeAttribute('disabled');
@@ -39,6 +14,3 @@ targetIdInput.addEventListener('input', () => {
         connectButton.style.cursor = 'not-allowed';
     }
 });
-
-// download 隐藏
-download.style.display = 'none';
