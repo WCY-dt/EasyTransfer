a0cfd8720f6c8aa39c8dd9d9e1f2f965d4381325
Ch3nyang <834421194@qq.com>
2024-12-04 16:12:02 +0800

    refactor: Move header and footer into vue file

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,33 @@
 <script setup>
+import HeaderCluster from './components/HeaderCluster.vue';
 import OperationCluster from '@/components/OperationCluster.vue'
 import SendCluster from '@/components/SendCluster.vue'
 import ReceiveCluster from '@/components/ReceiveCluster.vue'
+import FooterCluster from './components/FooterCluster.vue';
 </script>
 
 <template>
-  <div class="app-cluster">
-    <OperationCluster />
-    <SendCluster />
-    <ReceiveCluster />
+  <HeaderCluster />
+  <div id="app-content">
+    <div class="app-cluster">
+      <OperationCluster />
+      <SendCluster />
+      <ReceiveCluster />
+    </div>
   </div>
+  <FooterCluster />
 </template>
 
 <style scoped lang="scss">
+#app-content {
+  display: flex;
+  align-items: start;
+  justify-content: center;
+
+  width: 100%;
+  min-height: 40vh;
+}
+
 .app-cluster {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
@@ -22,7 +37,7 @@ import ReceiveCluster from '@/components/ReceiveCluster.vue'
   width: min(96vw, 120rem);
   margin: 2rem 2vw;
 
-  & > * {
+  &>* {
     width: calc((min(96vw, 120rem) - 4rem) / 3);
   }
 }
@@ -32,7 +47,7 @@ import ReceiveCluster from '@/components/ReceiveCluster.vue'
     grid-template-columns: 1fr 1fr;
     grid-template-rows: auto auto;
 
-    & > * {
+    &>* {
       width: calc((min(96vw, 120rem) - 2rem) / 2);
     }
   }
@@ -45,7 +60,7 @@ import ReceiveCluster from '@/components/ReceiveCluster.vue'
 
     gap: 4rem;
 
-    & > * {
+    &>* {
       width: 96vw;
     }
   }
