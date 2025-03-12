06f48b1ba6695ba7a623f04e518ec729c0257ec9
Ch3nyang <834421194@qq.com>
2025-03-12 15:25:49 +0800

    feat: STUN/TURN servers will reset on new version release

--- client/src/const.ts
+++ client/src/const.ts
@@ -4,38 +4,19 @@ export const defaultMaxConnectionNumber: number = 10
 
 export const defaultIceServers: IceServer[] = [
   {
-    urls: 'stun:stun.l.google.com:19302',
-  },
-  {
-    urls: 'stun:stun.l.google.com:5349',
-  },
-  {
-    urls: 'stun:stun1.l.google.com:3478',
-  },
-  {
-    urls: 'stun:stun1.l.google.com:5349',
-  },
-  {
-    urls: 'stun:stun2.l.google.com:19302',
-  },
-  {
-    urls: 'stun:stun2.l.google.com:5349',
-  },
-  {
-    urls: 'stun:stun3.l.google.com:3478',
-  },
-  {
-    urls: 'stun:stun3.l.google.com:5349',
-  },
-  {
-    urls: 'stun:stun4.l.google.com:19302',
-  },
-  {
-    urls: 'stun:stun4.l.google.com:5349',
-  },
-  {
     urls: 'turn:103.124.107.241:3478',
     username: 'easytransfer',
     credential: 'sharesimplyandstayanonymous',
+  },
+  {
+    urls: 'stun:stun.relay.metered.ca:80',
+  },
+  {
+    urls: 'turn:global.relay.metered.ca:80',
+    username: 'cf841207b56ebddc17948dde',
+    credential: '0dGvvEm7eq2UaqlW',
+  },
+  {
+    urls: 'stun:stun.l.google.com:19302',
   },
 ]
